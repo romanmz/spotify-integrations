40f24f3247019e5705f090979bf198fa2e8f7d14
Román Martínez <roman.mx3@gmail.com>
2019-02-20 14:47:41 -0600

    Init as WordPress plugin

---
+++ spotify-integrations.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Spotify Integrations
+Description: Get information about your Spotify library
+Author: Roman Martinez
+Author URI: https://romanmartinez.me/
+Version: 1.0
+*/
+
+defined( 'ABSPATH' ) or die( 'No direct access allowed' );
